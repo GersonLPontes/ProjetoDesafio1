b32d8e5e75c00c79cc3e730a4c352be4e8b6783a
GersonPontes <gersonpontes11@gmail.com>
2023-12-13 09:04:17 -0300

    Criação do projeto

---
+++ src/main/java/com/devsuperior/ProjetoDesafio1/ProjetoDesafio1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ProjetoDesafio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDesafio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDesafio1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/ProjetoDesafio1/ProjetoDesafio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ProjetoDesafio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDesafio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
